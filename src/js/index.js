364f27dce722ca1f37c0885d55a71e0997530d2d
Douglas Braz <90809043+DouglasBraz07@users.noreply.github.com>
2022-07-31 07:02:51 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+
+const btnAvancar = document.getElementById('btn-avancar');
+const btnVoltar = document.getElementById('btn-voltar');
+const cartoes = document.querySelectorAll('.cartao'); 
+let cartaoAtual = 0;
+
+function esconderCartaoSelecionado(){
+    const cartaoSelecionado = document.querySelector('.selecionado');
+    cartaoSelecionado.classList.remove('selecionado');
+}
+
+function mostraCartao(indiceCartao){
+    cartoes[indiceCartao].classList.add('selecionado');
+}
+
+btnAvancar.addEventListener('click', function () {
+
+    if(cartaoAtual === cartoes.length - 1) return;
+
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual++;
+    mostraCartao(cartaoAtual); 
+    
+})
+
+btnVoltar.addEventListener('click', function () {
+
+    if(cartaoAtual === 0) return;
+
+    esconderCartaoSelecionado();
+    
+    cartaoAtual--;
+    mostraCartao(cartaoAtual);
+
+});
